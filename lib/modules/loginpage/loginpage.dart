9e05f1c8191584947b0e04a472d03a83d67f0286
alvinow <alvinowijaksana@gmail.com>
2025-01-07 17:52:49 +0700

    #4 commit

---
+++ lib/modules/loginpage/loginpage.dart
@@ -0,0 +1,244 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:matrixclient/appconfig.dart';
+import 'package:matrixclient/modules/edokumen2022/formdefinitionlib/formdefinitionlib.dart';
+import 'package:matrixclient/modules/edokumen2022/pagecoordinator/bloc/pagecoordinator_bloc.dart';
+import 'package:matrixclient/modules/edokumen2022/rowdefinitionlib/rowdefinitionlib.dart';
+import 'package:matrixclient/modules/util/vwdateutil.dart';
+import 'package:matrixclient/modules/vwform/vwform.dart';
+import 'package:image_fade/image_fade.dart';
+import 'package:matrixclient/modules/vwform/vwformdefinition/vwformdefinition.dart';
+import 'package:matrixclient/modules/vwformpage/vwdefaultformpage.dart';
+import 'package:matrixclient2base/modules/base/vwappinstanceparam/vwappinstanceparam.dart';
+import 'package:matrixclient2base/modules/base/vwdataformat/vwfiedvalue/vwfieldvalue.dart';
+import 'package:matrixclient2base/modules/base/vwdataformat/vwrowdata/vwrowdata.dart';
+import 'package:uuid/uuid.dart';
+
+class LoginPage extends StatefulWidget {
+  LoginPage(
+      {super.key,
+      required this.appInstanceParam,
+      required this.paramLoginPage});
+
+  final VwRowData paramLoginPage;
+  final VwAppInstanceParam appInstanceParam;
+
+  LoginPageState createState() => LoginPageState();
+}
+
+class LoginPageState extends State<LoginPage> {
+  late VwRowData currentVwRow;
+
+  void _implementOnFormFieldValueChanged(
+      VwFieldValue newValue,
+      VwFieldValue oldValue,
+      VwRowData currentFormResponse,
+      bool isDoSetState) {}
+  late Key formPageKey;
+  late Key googleButtonKey;
+  late Key signUpKey;
+
+  @override
+  void initState() {
+    super.initState();
+    this.signUpKey = UniqueKey();
+    this.googleButtonKey = UniqueKey();
+    this.formPageKey = UniqueKey();
+    currentVwRow = this.widget.paramLoginPage;
+  }
+
+  static String getApplicationName(){
+    String returnValue=AppConfig.appTitle;
+
+    try {
+
+      if(Uri.base.path=="/tinjut")
+        {
+          returnValue="SIM Tindak Lanjut Audit 2024";
+        }
+      else  if(Uri.base.path=="/edok")
+        {
+          returnValue="e-Dokumen 2023";
+        }
+
+      print("Uri Base Host: " + Uri.base.host);
+      print("Uri Base Path" + Uri.base.path);
+    }
+    catch(error)
+    {
+
+    }
+
+    return returnValue;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    String appName = LoginPageState.getApplicationName();
+    Widget titleCaption = Container(
+        margin: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            /*Container(
+                margin: const EdgeInsets.fromLTRB(0, 0, 10, 0),
+                child: const FaIcon(
+                  FontAwesomeIcons.folderClosed,
+                  color: Colors.blue,
+                  size: 26,
+                )),*/
+            Text(
+              appName,
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w800),
+            )
+          ],
+        ));
+
+    Widget titleLogo = Container(
+      margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
+      child: Container(
+          height: 125,
+          child: ImageFade(
+            syncDuration: Duration(seconds: 1),
+            // here `bytes` is a Uint8List containing the bytes for the in-memory image
+            //scale: 1,
+            image: AssetImage(
+              AppConfig.mainLogoPath,
+            ),
+          )),
+    );
+
+    Widget versionWidget = Text(
+      'Ver. ' + AppConfig.appVersion,
+      style: TextStyle(color: Colors.blueGrey, fontSize: 12),
+    );
+
+    Widget title = Column(
+      children: [
+        titleLogo,
+        titleCaption,
+        versionWidget
+      ],
+    );
+
+    Widget submitButton =
+        Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+      ElevatedButton.icon(
+          style: ElevatedButton.styleFrom(
+              foregroundColor: Colors.white, backgroundColor: Colors.blue),
+          onPressed: () {
+            String username =
+                currentVwRow.getFieldByName('username')!.getValueAsString();
+            String password =
+                currentVwRow.getFieldByName('password')!.getValueAsString();
+
+            VwRowData loginRow = RowDefinitionLib.convertRowDefinitionToRow(
+                rowDefinition: RowDefinitionLib.getLoginRequestRowDefinition());
+
+            VwFieldValue? usernameField = loginRow.getFieldByName('username');
+
+            usernameField?.valueString = username;
+
+            VwFieldValue? passwordField = loginRow.getFieldByName('password');
+            passwordField?.valueString = password;
+
+            this.widget.appInstanceParam.appBloc.add(
+                AuthorizePagecoordinatorEvent(
+                    timestamp: DateTime.now(), paramAuthorize: loginRow));
+          },
+          icon: const Icon(Icons.login),
+          label: const Text('Log In'))
+    ]);
+
+    Widget signUpButton = ElevatedButton.icon(
+      onPressed: () async {
+        Widget userSignUpFormPage = VwFormPage(
+            key: this.signUpKey,
+            syncCaption: "Kirim",
+            formName: "Daftar Akun",
+            enableTitle: false,
+            loadFormDefinitionFormServer: true,
+            formResponse: VwRowData(recordId: Uuid().v4()),
+            formDefinition: VwFormDefinition(
+                loadDetailFromServer: false,
+                formResponseSyncCollectionName: "userquorasignupformdefinition",
+                sections: [],
+                timestamp: VwDateUtil.nowTimestamp(),
+                formName: "Daftar Akun",
+                recordId: "userquorasignupformdefinition"),
+            appInstanceParam: widget.appInstanceParam,
+            formDefinitionFolderNodeId: "userquorasignupformdefinition");
+
+        await showModalBottomSheet(
+            backgroundColor: Colors.transparent,
+            isScrollControlled: true,
+            isDismissible: false,
+            useSafeArea: true,
+            context: context,
+            builder: (context) => userSignUpFormPage);
+      },
+      style: ElevatedButton.styleFrom(
+          foregroundColor: Colors.blue, backgroundColor: Colors.white),
+      label: const Text('Daftar/Sign In email OTP '),
+      icon: const Icon(Icons.mail_outline_rounded),
+    );
+
+    //Widget googleSignInButton=Container( width:270,height:50,child:ButtonConfiguratorDemo(key:this.googleButtonKey, appInstanceParam: this.widget.appInstanceParam));
+
+    Widget googleSignInButton = Container();
+
+    BoxDecoration boxDecoration1 = BoxDecoration(
+        color: Colors.white,
+        border: Border.all(color: Colors.blue, width: 0.5),
+        borderRadius: BorderRadius.all(Radius.circular(2))
+        //borderRadius: BorderRadius.circular(10.0),
+        );
+
+    Widget loginForm = VwForm(
+        key: this.formPageKey,
+        appInstanceParam: widget.appInstanceParam,
+        boxDecoration: boxDecoration1,
+        onFormValueChanged: _implementOnFormFieldValueChanged,
+        backGroundColor: Colors.white,
+        initFormResponse: currentVwRow,
+        formDefinition: FormDefinitionLib.getLoginForm());
+
+    //form =Text("Hello World!");
+
+    Widget loginBox=Container(
+
+      margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
+      color: Colors.white,
+      constraints: BoxConstraints(maxWidth: 350),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(height: 10,),
+          title,
+
+          loginForm,
+          Container(
+            child: submitButton,
+            margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
+          ),
+        ],
+      ),
+    );
+
+    return Scaffold(
+        backgroundColor: Colors.grey,
+        key: this.widget.key,
+        body:
+        SingleChildScrollView(
+          //physics: AlwaysScrollableScrollPhysics(),
+          child:
+          Center(child: Stack(alignment: Alignment.center, children: [loginBox],),)
+
+        ),
+    );
+  }
+}
---
+++ lib/modules/mediaviewerpage/mediaviewerpage.dart
@@ -0,0 +1,442 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:matrixclient/appconfig.dart';
+import 'package:matrixclient/modules/edokumen2022/remoteapi/remote_api.dart';
+import 'package:matrixclient/modules/util/nodeutil.dart';
+import 'package:matrixclient/modules/util/widgetutil.dart';
+import 'package:matrixclient/modules/vwwidget/nodelistview/nodelistview.dart';
+import 'package:matrixclient/modules/vwwidget/vwformresponseuserpage/vwformresponseuserpage.dart';
+import 'package:matrixclient/modules/youtubeappdemo/youtubeappdemo.dart';
+import 'package:matrixclient2base/modules/base/vwapicall/apivirtualnode/apivirtualnode.dart';
+import 'package:matrixclient2base/modules/base/vwappinstanceparam/vwappinstanceparam.dart';
+import 'package:matrixclient2base/modules/base/vwdataformat/vwfiedvalue/vwfieldvalue.dart';
+import 'package:matrixclient2base/modules/base/vwdataformat/vwrowdata/vwrowdata.dart';
+import 'package:matrixclient2base/modules/base/vwlinknode/vwlinknode.dart';
+import 'package:matrixclient2base/modules/base/vwnode/vwnode.dart';
+import 'package:matrixclient2base/modules/base/vwnoderequestresponse/vwnoderequestresponse.dart';
+import 'package:uuid/uuid.dart';
+
+typedef BackPageFunction = void Function(
+    VwAppInstanceParam appInstanceParam, BuildContext buildContext);
+
+class MediaViewerPage extends StatefulWidget {
+  const MediaViewerPage(
+      {super.key,
+      required this.mediaLinkNode,
+      required this.appInstanceParam,
+      this.backPageFunction,
+      this.autoplay = false,
+      this.mediaPointer});
+
+  final VwLinkNode mediaLinkNode;
+  final VwAppInstanceParam appInstanceParam;
+  final BackPageFunction? backPageFunction;
+  final bool autoplay;
+  final VwRowData? mediaPointer;
+
+  static const String fsInstanceInitiated = ' fsInstanceInitiated';
+  static const String fsNodeLoaded = ' fsNodeLoaded';
+  static const String errorLoadingNode = 'errorLoadingNode';
+  static const String fsLoadingNode = 'fsLoadingNode';
+  static const String fsNodeNotExits = 'fsNodeNotExits';
+
+  MediaViewerPageState createState() => MediaViewerPageState();
+}
+
+class MediaViewerPageState extends State<MediaViewerPage> {
+  late Key currentMediaKey;
+  VwRowData? currentMediaPointer;
+  late bool enableAppBar;
+
+  late String currentLoadStatus;
+  VwNode? currentMediaNode;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    this.currentMediaNode = NodeUtil.getNode(linkNode: widget.mediaLinkNode);
+    this.generateMediaKey();
+    this.enableAppBar = true;
+    this.currentLoadStatus = MediaViewerPage.fsInstanceInitiated;
+    this.currentMediaPointer = widget.mediaPointer;
+  }
+
+  void defaultImplementBackFunction(
+      VwAppInstanceParam appInstanceParam, BuildContext buildContext) {
+    Navigator.pop(buildContext);
+  }
+
+  VwRowData? getArticleRowData() {
+    VwRowData? returnValue;
+    try {
+      if (this.currentMediaNode != null &&
+          this.currentMediaNode!.content != null &&
+          this.currentMediaNode!.content!.rowData != null)
+        returnValue = this.currentMediaNode!.content!.rowData!;
+    } catch (error) {}
+    return returnValue;
+  }
+
+  void generateMediaKey() {
+    this.currentMediaKey = UniqueKey();
+  }
+
+  Widget getYoutubeWidget({VwRowData? videoPointer}) {
+    Widget returnValue = Container();
+    try {
+      String videoId = this
+          .getArticleRowData()!
+          .getFieldByName("medialinktitle")!
+          .valueString!;
+      returnValue = YoutubeAppDemo(
+          autoplay: widget.autoplay, key: currentMediaKey, videoIds: [videoId]);
+      if (videoPointer != null) {
+        try {
+          double startSeconds =
+              videoPointer.getFieldByName("startseconds")!.valueNumber!;
+          double endSeconds =
+              videoPointer.getFieldByName("endseconds")!.valueNumber!;
+
+          returnValue = YoutubeAppDemo(
+              key: currentMediaKey,
+              startSeconds: startSeconds,
+              endSeconds: endSeconds,
+              autoplay: true,
+              videoIds: [videoId]);
+        } catch (error) {}
+      }
+    } catch (error) {}
+    return returnValue;
+  }
+
+  bool getEnableCommentBoxBottomSide() {
+    bool returnValue = false;
+    try {
+      if (this.widget.appInstanceParam.loginResponse != null &&
+          this.widget.appInstanceParam.loginResponse!.userInfo != null) {
+        returnValue = true;
+      }
+    } catch (error) {}
+    return returnValue;
+  }
+
+  void implementCommandToParentFunction(VwRowData callParameter) {
+    generateMediaKey();
+    this.currentMediaPointer = callParameter;
+    setState(() {});
+  }
+
+  Widget getFeedFolderNode() {
+    /*
+    return Scaffold(
+      appBar: AppBar(automaticallyImplyLeading: true,),
+      body:Text("test App Bar")
+    );*/
+
+    return VwFormResponseUserPage(
+      commandToParentFunction: this.implementCommandToParentFunction,
+      parentArticleNode: NodeUtil.getNode(linkNode: widget.mediaLinkNode),
+      boxTopRowWidgetMode: NodeListView.btrDisabled,
+      bottomSideMode: getEnableCommentBoxBottomSide()
+          ? NodeListView.bsmVideoPointerBox
+          : NodeListView.bsmDisabled,
+      zeroDataCaption: "(Belum Ada Janji)",
+      enableAppBar: true,
+      showBackArrow: false,
+      enableScaffold: true,
+      showUserInfoIcon: false,
+      showLoginButton: false,
+      toolbarHeight: 35,
+      toolbarPadding: 3,
+      rowUpperPadding: 35,
+      mainHeaderTitleTextColor: Colors.black,
+      mainHeaderBackgroundColor: Colors.white,
+      enableCreateRecord: false,
+      mainLogoTextCaption: "Janji",
+      folderNodeId: "15b492f8-e4fb-496d-a999-a4afc39bc184",
+      key: this.widget.key,
+      appInstanceParam: widget.appInstanceParam,
+      showReloadButton: false,
+      extendedApiCallParam: VwRowData(recordId: Uuid().v4(), fields: [
+        VwFieldValue(
+            fieldName: "parentarticlenodeid",
+            valueString: this.widget.mediaLinkNode.nodeId)
+      ]),
+    );
+  }
+
+  String getLoginSessionId() {
+    String returnValue = AppConfig.loginSessionGuestUserId;
+    try {
+      returnValue = widget.appInstanceParam.loginResponse!.loginSessionId!;
+    } catch (error) {}
+
+    return returnValue;
+  }
+
+  Future<VwNode?> _asyncLoadNode() async {
+    VwNode? returnValue;
+    this.currentLoadStatus = MediaViewerPage.fsLoadingNode;
+    try {
+      returnValue = this.currentMediaNode;
+      if (returnValue == null) {
+        VwRowData apiCallParam = VwRowData(recordId: Uuid().v4(), fields: [
+          VwFieldValue(
+            fieldName: "nodeId",
+            valueString: APIVirtualNode.exploreQuestionFeed,
+          ),
+          VwFieldValue(
+              fieldName: "parentarticlenodeid",
+              valueString: widget.mediaLinkNode.nodeId),
+        ]);
+
+        VwNodeRequestResponse nodeRequestResponse =
+            await RemoteApi.nodeRequestApiCall(
+                apiCallId: "getNodes",
+                apiCallParam: apiCallParam,
+                loginSessionId: this.getLoginSessionId());
+
+        if (nodeRequestResponse.httpResponse != null &&
+            nodeRequestResponse.httpResponse!.statusCode == 200) {
+          if (nodeRequestResponse.renderedNodePackage != null &&
+              nodeRequestResponse.renderedNodePackage!.parentArticleNode !=
+                  null) {
+            this.currentMediaNode =
+                nodeRequestResponse.renderedNodePackage!.parentArticleNode!;
+            returnValue = this.currentMediaNode;
+
+            try {
+              if (returnValue!.recordId != this.widget.mediaLinkNode.nodeId) {
+                for (int la = 0;
+                    la <
+                        nodeRequestResponse
+                            .renderedNodePackage!.renderedNodeList!.length;
+                    la++) {
+                  VwNode currentNode = nodeRequestResponse
+                      .renderedNodePackage!.renderedNodeList!
+                      .elementAt(la);
+
+                  if (currentNode.recordId ==
+                      this.widget.mediaLinkNode.nodeId) {
+                    this.currentMediaPointer = currentNode.content.rowData;
+                    break;
+                  }
+                }
+              }
+            } catch (error) {}
+
+            this.currentLoadStatus = MediaViewerPage.fsNodeLoaded;
+          } else {
+            this.currentLoadStatus = MediaViewerPage.fsNodeNotExits;
+          }
+        } else {
+          this.currentLoadStatus = MediaViewerPage.errorLoadingNode;
+        }
+      } else {
+        this.currentLoadStatus = MediaViewerPage.fsNodeLoaded;
+      }
+    } catch (error) {}
+
+    return returnValue;
+  }
+
+  Widget _reloadFormWithScaffold(String caption) {
+    return Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: Center(child: this._reloadForm(caption)));
+  }
+
+  Widget _reloadForm(String caption) {
+    Widget body = InkWell(
+        onTap: () {
+          setState(() {
+            this.currentLoadStatus = MediaViewerPage.fsLoadingNode;
+          });
+        },
+        child: Container(
+            color: Colors.blue,
+            child: Icon(Icons.refresh, color: Colors.white, size: 60)));
+
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        body,
+        Container(margin: EdgeInsets.all(10), child: Text(caption))
+      ],
+    );
+  }
+
+  Widget _builReloadForm(String caption) {
+    return this._reloadFormWithScaffold(caption);
+  }
+
+  Widget buildMediaViewer() {
+    return LayoutBuilder(builder: (buildContext, boxConstraint) {
+      bool isPortrait = true;
+
+      double ratio = boxConstraint.maxWidth / boxConstraint.maxHeight;
+
+      if (ratio > 1.2) {
+        isPortrait = false;
+      }
+
+      String title =
+          this.getArticleRowData()!.getFieldByName("title")!.valueString!;
+      if(this.currentMediaPointer!=null)
+        {
+          try {
+            title =
+            this.currentMediaPointer!.getFieldByName("title")!.valueString!;
+          }
+          catch(error)
+      {
+
+      }
+        }
+
+      if (isPortrait) {
+        //this.enableAppBar=true;
+
+        double youtubeHeight = boxConstraint.maxWidth / (16 / 9);
+        double youtubeWidth = boxConstraint.maxWidth;
+        double ratio = youtubeHeight / boxConstraint.maxHeight;
+        if (ratio > 0.6) {
+          youtubeWidth = youtubeWidth / 2;
+        }
+
+        return Scaffold(
+            key: this.widget.key,
+            appBar: this.enableAppBar == true
+                ? PreferredSize(
+                    preferredSize: Size.fromHeight(50.0),
+                    child: AppBar(
+                      backgroundColor: AppConfig.primaryColor,
+                      title: Text(
+                        title,
+                        style: TextStyle(color: AppConfig.textColor),
+                      ),
+                      leading: InkWell(
+                        onTap: () {
+                          if (widget.backPageFunction != null) {
+                            widget.backPageFunction!(
+                                widget.appInstanceParam, context);
+                          } else {
+                            this.defaultImplementBackFunction(
+                                widget.appInstanceParam, context);
+                          }
+                        },
+                        child: Icon(
+                          Icons.arrow_back,
+                          size: 20,
+                          color: AppConfig.textColor,
+                        ),
+                      ),
+                    ))
+                : null,
+            body: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                    alignment: Alignment.center,
+                    constraints: BoxConstraints(maxWidth: youtubeWidth),
+                    child: this
+                        .getYoutubeWidget(videoPointer: currentMediaPointer)),
+                Expanded(
+                  child: Container(
+                    child: getFeedFolderNode(),
+                  ),
+                )
+              ],
+            ));
+      } else {
+        double youtubeMaxWidth =
+            (boxConstraint.maxHeight * (16 / 9)) - (35 * 16 / 9);
+
+        double youtubeMaxHeighth = boxConstraint.maxHeight;
+
+        if (ratio < 2.4) {
+          youtubeMaxWidth = (1 - (0.45 * (2.4 - ratio))) * youtubeMaxWidth;
+        }
+
+        return Scaffold(
+            key: this.widget.key,
+            appBar: this.enableAppBar == true
+                ? PreferredSize(
+                    preferredSize: Size.fromHeight(35.0),
+                    child: AppBar(
+                      backgroundColor: AppConfig.primaryColor,
+                      title: Text(
+                        title,
+                        style: TextStyle(color: AppConfig.textColor),
+                      ),
+                      leading: InkWell(
+                        onTap: () {
+                          if (widget.backPageFunction != null) {
+                            widget.backPageFunction!(
+                                widget.appInstanceParam, context);
+                          } else {
+                            this.defaultImplementBackFunction(
+                                widget.appInstanceParam, context);
+                          }
+                        },
+                        child: Icon(
+                          Icons.arrow_back,
+                          size: 20,
+                          color: AppConfig.textColor,
+                        ),
+                      ),
+                    ))
+                : null,
+            body: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    LayoutBuilder(builder: (buildContext, boxConstraintColumn) {
+                      return Container(
+                          alignment: Alignment.topCenter,
+                          constraints:
+                              BoxConstraints(maxWidth: youtubeMaxWidth),
+                          child: this.getYoutubeWidget(
+                              videoPointer: currentMediaPointer));
+                    })
+                  ],
+                ),
+                Expanded(child: getFeedFolderNode())
+              ],
+            ));
+      }
+    });
+  }
+
+  Widget _buildLoadingWidget(String caption) {
+    return WidgetUtil.loadingWidget(caption);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<VwNode?>(
+        key: this.widget.key,
+        future: this._asyncLoadNode(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData == true) {
+            if (this.currentLoadStatus == MediaViewerPage.fsNodeLoaded) {
+              return this.buildMediaViewer();
+            } else if (this.currentLoadStatus ==
+                MediaViewerPage.errorLoadingNode) {
+              return this
+                  ._builReloadForm("Error : Can't connect to server");
+            } else {
+              return _buildLoadingWidget("Loading from server...");
+            }
+          } else {
+            return _buildLoadingWidget("Loading from server...");
+          }
+        });
+  }
+}
